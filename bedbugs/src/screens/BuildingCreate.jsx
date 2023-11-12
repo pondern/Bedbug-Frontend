99a43dcad01921ee99ace28faa8a07aa044d207d
pondern <nicolasponder@gmail.com>
2023-11-12 18:31:43 -0500

    add BuildingCreate.jsx

---
+++ bedbugs/src/screens/BuildingCreate.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createBuilding } from "../services/buildings";
+
+function BuildingCreate() {
+  const [building, setBuilding] = useState({
+    bldgadd: "",
+  });
+
+  let navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await createBuilding(building);
+    navigate("/buildings");
+  };
+
+  const handleChange = (e) => {
+    const { bldgadd, value } = e.target;
+
+    setBuilding((prevBuilding) => ({
+      ...prevBuilding,
+      [bldgadd]: value,
+    }));
+  };
+
+  return (
+    <div>
+      <h1>Add a building in our Database!</h1>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Please add your building's adress"
+          bldgadd="address"
+          value={building.bldgadd}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default BuildingCreate;
